dd4a9632c92fcaed67cadf2a9d9d7840bca60a66
mistertyty <loic.henaut@orange.fr>
2023-11-17 11:03:56 +0100

    toon shader fin

--- jeu loulou 16/Assets/shaders/toonshader.hlsl
+++ jeu loulou 16/Assets/shaders/toonshader.hlsl
@@ -1,4 +1,16 @@
 void ToonShading_float(in float3 Normal, in float ToonRampSmoothness, in float3 ClipSpacePos, in float3 worldPos, in float4 ToonRampTinting,
 in ToonRampOffset , out float3 ToonRampOutput, out float3 Direction)
 {
+   #ifdef SHADERGRAPH_PREVIEW
+    ToonRampOutput=float3 (0.5,0.5,0);
+    Direction=float3 (0.5,0.5,0);
+    #else 
+        #if SHADOW_SCREEN
+        half4 shadowCoord= ComputeScreenPos(ClipSpacePos);
+        #else
+        half4 shadowCoord = TranformWorldToShadowCoord(worldPos);
+    #endif
+    
+    
+
 }
