cc5bd51d5d6fc93134c2e77c0908bbaaa0601758
mistertyty <loic.henaut@orange.fr>
2023-11-17 08:18:10 +0100

    debut code shader

---
+++ jeu loulou 16/Assets/shaders/toonshader.hlsl
@@ -0,0 +1,4 @@
+void ToonShading_float(in float3 Normal, in float ToonRampSmoothness, in float3 ClipSpacePos, in float3 worldPos, in float4 ToonRampTinting,
+in ToonRampOffset , out float3 ToonRampOutput, out float3 Direction)
+{
+}
